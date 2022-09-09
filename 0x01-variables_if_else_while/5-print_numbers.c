dc875f2dea057b35762ce9db3983bbf5d51ffb67
Ekozini <oruosah>
2022-09-09 14:06:58 -0700

    made'

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - printsdodsjfD
+ * DESCRIPTION: USING THR MAIN FUNTION 
+ * ths program would print "adkfkasdjfkjasdf
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+	printf("%i", i);
+}
+printf('\n');
+return (0);
+}
