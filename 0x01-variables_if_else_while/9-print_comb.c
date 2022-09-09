00317beeb7d05eb86ee8ee09c2a3e9511bf7cf83
Ekozini <oruosah>
2022-09-09 16:48:54 -0700

    Frapacciono

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - this is a program
+ * 
+ * Description: tljahljasdlkafl
+ * withwt heh piwer "kjdhgkjadhflgjajdhl
+ * Return: 0
+ */
+int main(void)
+
+{
+	int c;
+	
+	for (c = 48 ; c <= 57 ; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
