d25065b634533c2791c87250951be9ddc33b0c22
Ekozini <oruosah>
2022-09-09 14:00:07 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints stuff
+ *
+ * Description: positive and makejlf p
+ * this prints "jajfkadfkkaj
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for(ch = 'a' ; ch <= 'z' ; ch++ )
+{
+if (ch != 'e' && ch != 'q')
+{
+	putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
