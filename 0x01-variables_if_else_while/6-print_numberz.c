b95ec7fc29296047772ae8ea8eb13ad1db0ebe0b
Ekozini <oruosah>
2022-09-09 16:19:16 -0700

    Made

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this prints stuff
+ * Description: using the main function
+ * this program prints " A hefty sum
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+for (c = 'z' ; c >= 'a' ; c--)
+{
+	putchar(c);
+}
+putchar("\n");
+return (0);
+}
