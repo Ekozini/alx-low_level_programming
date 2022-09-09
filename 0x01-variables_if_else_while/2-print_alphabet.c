bebfc755e80d6422b85dc99a71575481b14a8708
Ekozini <oruosah>
2022-09-09 13:20:33 -0700

    Char

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - this program prints postive or negative
+ * Description: using ths main function
+ * this program prints "Positive, negative, zero
+ * Returns: o
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
