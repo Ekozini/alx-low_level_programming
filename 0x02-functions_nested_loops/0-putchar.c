20cefdabc156a7477721e7bbe74c42a1cf5d1e22
Ekozini <oruosah>
2022-09-13 12:26:55 -0700

    Made

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - using the main function
- *
- * Description: using the main function 
- * this program would print "Printchar as a string
- * Return: 0
+ * main - print putchar
+ * Return: always 0
  */
 
 int main(void)
 {
-	printf("_putchar\n");
-	return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0); 
 }
